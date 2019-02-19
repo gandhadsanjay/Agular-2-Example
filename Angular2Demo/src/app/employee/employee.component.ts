eb6b9c37a64c98b8a4f36b7c45086d9995e19e51
gandhads <sanjay.gandhad@powerschool.com>
2019-02-19 18:07:21 +0530

    Angular Attribute binding using attr

--- Angular2Demo/src/app/employee/employee.component.ts
+++ Angular2Demo/src/app/employee/employee.component.ts
@@ -5,6 +5,7 @@
     templateUrl:"app/employee/employee.component.html"
 })
 export class EmployeeComponent {
+    colSpan: number = 2;
     firstName: string = "Sanjay";
     lastName: string = "Gandhad";
     gender: string = "Male";
