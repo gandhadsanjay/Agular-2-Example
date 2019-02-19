7ef1fcfb4991cfa2bcd8b77f1f91e4681c8141c6
gandhads <sanjay.gandhad@powerschool.com>
2019-02-19 18:46:06 +0530

    CSS binding in angular

--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -1,7 +1,36 @@
 import { Component } from '@angular/core';
+import { transition } from '@angular/core/src/animation/dsl';
+import { fail } from 'assert';
 
 @Component({
-  selector: 'my-app',
-    templateUrl: `app/app.component.html`
+    selector: 'my-app',
+    template: `<div> 
+                <button class='boldClass' text='My Button'>My Button</button>  
+                <br/><br/>
+                <button [class.boldClass]='isboldclass' text='My Button'>My Button</button> 
+                <br/><br/>
+                <button [class]='applyiedClass' text='My Button'>My Button</button> 
+                <br/><br/>
+                <button [ngClass]='applyClass()' text='My Button'>My Button</button>
+                </div>`
 })
-export class AppComponent  { pageHeader = 'Angular component nesting'; }
+
+export class AppComponent  {
+    pageHeader = 'Angular component nesting';
+    isboldclass: boolean = true; 
+    isItalics: boolean = false;
+    isColorApplyied: boolean = true;
+    applyiedClass: string = " boldClass italicsClass colorClass";
+
+    applyClass()
+    {
+        let classes = {
+            boldClass: this.isboldclass,
+            italicsClass: this.isItalics,
+            colorClass: this.isColorApplyied
+        }
+        return classes;
+    }
+
+
+}
