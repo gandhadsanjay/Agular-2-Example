5882d78e6401aea19f34756dc6a20f727a861f42
gandhads <sanjay.gandhad@powerschool.com>
2019-02-20 18:37:33 +0530

    Anjular style binding

--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -1,35 +1,38 @@
 import { Component } from '@angular/core';
 import { transition } from '@angular/core/src/animation/dsl';
 import { fail } from 'assert';
+import { normalize } from 'path';
 
 @Component({
     selector: 'my-app',
     template: `<div> 
-                <button class='boldClass' text='My Button'>My Button</button>  
+                <button style='font-weight:bold' text='My Button'>My Button</button>  
                 <br/><br/>
-                <button [class.boldClass]='isboldclass' text='My Button'>My Button</button> 
+                <button [style.color]='applyColor' text='My Button'>My Button</button> 
                 <br/><br/>
-                <button [class]='applyiedClass' text='My Button'>My Button</button> 
+                <button [style.font-size.px]='pixels' text='My Button'>My Button</button> 
                 <br/><br/>
-                <button [ngClass]='applyClass()' text='My Button'>My Button</button>
+                <button [ngStyle]='applyStyle()' text='My Button'>My Button</button>
                 </div>`
 })
 
 export class AppComponent  {
     pageHeader = 'Angular component nesting';
+    pixels: number = 50;
+    applyColor: string = "red";
     isboldclass: boolean = true; 
     isItalics: boolean = false;
     isColorApplyied: boolean = true;
     applyiedClass: string = " boldClass italicsClass colorClass";
 
-    applyClass()
+    applyStyle()
     {
-        let classes = {
-            boldClass: this.isboldclass,
-            italicsClass: this.isItalics,
-            colorClass: this.isColorApplyied
+        let styles = {
+            'font-weight': this.isboldclass ?'bold':'normal',
+           // 'font-style:italcs': this.isItalics,
+            'color': this.isColorApplyied ? 'blue': ''
         }
-        return classes;
+        return styles;
     }
 
 
